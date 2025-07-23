137701f9cc38e8737686102234f5be6e284492b7
ksfallon <fallon.katelin@gmail.com>
2025-07-23 16:28:07 -0400

    Add project files.

---
+++ UserCreationFormApp/Models/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserCreationFormApp.Models
+{
+    public class User
+    {
+        public int ID { get; set; }
+        public required string FirstName { get; set; }
+        public required string LastName { get; set; }
+        public required string Email { get; set; }
+        public string? Phone {  get; set; }
+        public string? Address { get; set; }
+        public required string CreatedAt {  get; set; }
+    }
+}
---
+++ UserCreationFormApp/Repositories/UserRepository.cs
@@ -0,0 +1,78 @@
+﻿using UserCreationFormApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Data.SqlClient;
+
+namespace UserCreationFormApp.Repositories
+{
+    public class UserRepository
+    {
+        private readonly string connectionString = "Data Source=localhost;Initial Catalog=UserLoginForm;Integrated Security=True;Trust Server Certificate=True";
+
+        public List<User> GetUsers()
+        {
+            var users = new List<User>();
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+
+                    string sql = "SELECT * FROM Users ORDER BY ID DESC";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            while (reader.Read())
+                            {
+                                User user = new()
+                                {
+                                    ID = reader.GetInt32(0),
+                                    FirstName = reader.GetString(1),
+                                    LastName = reader.GetString(2),
+                                    Email = reader.GetString(3),
+                                    Phone = reader.GetString(4),
+                                    Address = reader.GetString(5),
+                                    CreatedAt = reader.GetDateTime(6).ToString()
+                                };
+
+                                users.Add(user);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) 
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+
+            return users;
+        }
+
+        public User? GetUser(int id)
+        {
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(connectionString))
+                {
+                    connection.Open();
+                    string sql = "SELECT * FROM Users WHERE ID=@ID";
+                    using (SqlCommand command = new SqlCommand(sql, connection))
+                    {
+
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Exception: " + ex.ToString());
+            }
+            return null;
+        }
+    }
+}
